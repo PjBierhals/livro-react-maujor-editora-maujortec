437358348c397d20195fd752d12c87facfee1d17
PjBierhals <pjbierhals@gmail.com>
2023-01-20 14:07:24 -0300

    Projeto 2 do livro primeiro passo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment >
+      <header className="topo">Topo</header>
+      <main className="principal">Principal</main>
+      <footer className="rodapé">Rodapé</footer>
+    </Fragment>
+  );
+}
+
+export default App;
