ea9b2345fafb2cb765eb1bd1375c986ee9125190
PjBierhals <pjbierhals@gmail.com>
2023-01-20 15:27:51 -0300

    Projeto 2 do livro terceiro passo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { Fragment } from "react";
+import Topo from "./components/Topo";
+import Principal from "./components/Principal";
+import Rodape from "./components/Rodape";
 
-function App() {
+const App = () => {
   return (
     <>
-      <header className="topo">
-        <h1 className="logo"></h1>
-        <ul>
-          <li><a href="">Navegação</a></li>
-        </ul>
-      </header>
-      <main className="principal">
-        <h2>Últimos Lanamentos</h2>
-        <div className="card">Card</div>
-      </main>
-      <footer className="rodape">
-        <p> Conteúdo cedidos pela Editora Novatec &#8212; Copyright 2020</p>
-      </footer>
+      <Topo />
+      <Principal />
+      <Rodape />
     </>
-  );
+
+  )
 }
 
 export default App;
---
+++ src/components/Topo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./Logo";
+import Navegacao from "./Navegacao";
+const Topo=()=>{
+    return(
+      <header className="topo">
+        <Logo/>
+        <Navegacao/>      
+      </header>  
+    );
+};
+export default Topo;
