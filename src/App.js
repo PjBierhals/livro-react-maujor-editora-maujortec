458a0318fbd7393327cec1e37e263af87f7ea3a3
PjBierhals <pjbierhals@gmail.com>
2023-01-20 14:26:23 -0300

    Projeto 2 do livro segundo  passo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Fragment } from "react";
 
 function App() {
   return (
-    <Fragment >
-      <header className="topo">Topo</header>
-      <main className="principal">Principal</main>
-      <footer className="rodapé">Rodapé</footer>
-    </Fragment>
+    <>
+      <header className="topo">
+        <h1 className="logo"></h1>
+        <ul>
+          <li><a href="">Navegação</a></li>
+        </ul>
+      </header>
+      <main className="principal">
+        <h2>Últimos Lanamentos</h2>
+        <div className="card">Card</div>
+      </main>
+      <footer className="rodape">
+        <p> Conteúdo cedidos pela Editora Novatec &#8212; Copyright 2020</p>
+      </footer>
+    </>
   );
 }
 
